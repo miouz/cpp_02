e26772246f3da7c46748b61238bf608b644e4c00
Mi Zhou <mzhou@student.42.fr>
2025-12-02 11:15:30 +0100

    update(ex02): update test

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -7,7 +7,7 @@ int	main()
 	Point a(-4.2,0);
 	Point b(4.34, 0);
 	Point c(0, 4);
-	Point p(1, 2.567309789686979890890809980696757879889890);
+	Point p(1, 2.56730);
 
 	std::cout << std::boolalpha << bsp(a, c, b, p)<< std::endl;	
 	std::cout << std::boolalpha << bsp(a, p, b, c)<< std::endl;	
